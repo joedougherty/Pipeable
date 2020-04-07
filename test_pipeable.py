31e6238c43065bde11f4360badb65783940834e7
Joe Dougherty <joseph.dougherty@gmail.com>
2020-04-07 19:18:56 -0400

    tests

--- test_pipeable.py
+++ test_pipeable.py
@@ -1,24 +1,18 @@
-### Pipeable: a Class Wrapper to make your functions magically pipeable ###
+from Pipeable import Pipeable
 
-    
-    from Pipeable import Pipeable
-        
 
+def test_it_does_what_it_says_it_does_on_the_box():
     def multiply_by_4(n):
         return n * 4
-
 
     def add_12(n):
         return n + 12
 
-
     def divide_by_2(n):
         return n / 2
 
-
     def minus_4(n):
         return n - 4
-
 
     times4 = Pipeable(multiply_by_4)
     plus12 = Pipeable(add_12)
@@ -26,12 +20,9 @@
     minus4 = Pipeable(lambda x: x - 4) # Yes, lambdas work as well!
 
     # BEHOLD
-    result = times4(5) | plus12 | halve | minus4
+    pipe_result = times4(5) | plus12 | halve | minus4
 
     # The above call yields the same answer as...
-    minus_4(divide_by_2(add_12(multiply_by_4(5))))
+    nested_call_result = minus_4(divide_by_2(add_12(multiply_by_4(5))))
 
-
-### Running the tests ###
-
-    pytest
+    assert(pipe_result.value == nested_call_result)
